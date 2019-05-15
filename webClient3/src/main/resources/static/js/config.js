47f539f1d7b5327520c89a2e80d376fb9a073352
Vu Thang <20156510@student.hust.edu.vn>
2019-05-15 14:06:16 +0700

    update bao cao

--- webClient3/src/main/resources/static/js/config.js
+++ webClient3/src/main/resources/static/js/config.js
@@ -5,7 +5,7 @@ var port_server_core = "8080";
 
 var protocol_client = "http";
 var host_client = "localhost";
-var port_client = "9999";
+var port_client = "8085";
 
 var id = -1;
 var userRole = -1;
